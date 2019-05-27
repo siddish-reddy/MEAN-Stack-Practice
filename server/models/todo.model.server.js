b32e750d0b0997b3572b51eb119d0ad7f2ee4d51
siddish-reddy <siddish.ibv@gmail.com>
2019-05-27 23:29:00 +0530

    todo item db model

--- server/models/todo.model.server.js
+++ server/models/todo.model.server.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const todoSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, default: " " },
-    dateAdded: { type: Date, required: true }
+  title: { type: String, required: true },
+  description: { type: String, default: " " }
 });
 module.exports = mongoose.model('Todo', todoSchema);
