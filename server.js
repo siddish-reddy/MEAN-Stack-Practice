1d1da18dcb55541724a769979c49e8b4f3cfef79
siddish-reddy <siddish.ibv@gmail.com>
2019-05-27 14:57:25 +0530

    fixed mongo warning and hanging API

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const config = require('./server/config');
 const mongoose = require('mongoose');
 
-mongoose.connect(config.MONGO_CONNECTION_URI)
+mongoose.connect(config.MONGO_CONNECTION_URI,  { useNewUrlParser: true })
   .then(()=>console.log("Connected to mongo db"))
   .catch(err=>console.error(err));
 
 const app = express();
+app.get('/api', (req, res)=>{
+  res.send('API working');
+});
 app.listen(8080, ()=>console.log("server started in 8080"));
