66130759a12e107d326938507ad143ca3952aabc
siddish-reddy <siddish.ibv@gmail.com>
2019-05-28 13:32:39 +0530

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'about', component: AboutComponent }
+];
+
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
