5ab6aa43288ca1208b6feb51efb117a99e6da678
siddish-reddy <siddish.ibv@gmail.com>
2019-05-28 09:43:47 +0530

    api working with mock values

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const config = require('./server/config');
 const mongoose = require('mongoose');
 const routes = require('./server/routes');
-mongoose.connect(config.MONGO_CONNECTION_URI,  { useNewUrlParser: true })
-  .then(()=>console.log("Connected to mongo db"))
-  .catch(err=>console.error(err));
 
+/*mongoose.connect(config.MONGO_CONNECTION_URI, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongo db"))
+  .catch(err => console.error(err));
+*/
 const app = express();
 app.use('/', routes);
-app.route('/').get();
-app.listen(8000, ()=>console.log("server started in 8000"));
+app.listen(8000, () => console.log("server started in 8000"));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const routes = express.Router();
 const Todo = require('../models/todo.model.server');
-
+const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var url = bodyParser.urlencoded();
 todos = [
   {
-    name:"Sample",
-    desc:"lorem ipsum",
-    date:'1122002'
+    name: "Sample",
+    desc: "lorem ipsum"
   }
 ];
-routes.get('/', (req, res)=>{
+routes.get('/', (req, res) => {
   console.log("/");
   res.status(200).json(todos);
 });
 
-routes.get('/api', (req, res)=>{
+routes.post('/', jsonParser, (req, res) => {
+  todos.push({ name: req.body.name, desc: req.body.desc });
+  res.status(200).json({ name: req.body.name, desc: req.body.desc });
+})
+
+routes.get('/api', (req, res) => {
   console.log("/api");
   res.status(200).send('API');
 });
